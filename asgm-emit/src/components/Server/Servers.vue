ecb5cf8ec4d6338195cd3c9079672d49109170be
ASANEE <asanee.tan@gmail.com>
2018-09-29 16:13:28 +0700

    assignment about pass data btw component

---
+++ asgm-emit/src/components/Server/Servers.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server-list
+                v-for="server in servers"
+                :server="server"></app-server-list>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import ServerList from './ServerList.vue';
+
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    { id: 1, status: 'Normal'},
+                    { id: 2, status: 'Critical'},
+                    { id: 3, status: 'Unknown'},
+                    { id: 4, status: 'Normal'}
+                ]
+            };
+        },
+        components: {
+            'app-server-list': ServerList
+        }
+    }
+</script>
+
+<style>
+
+</style>
