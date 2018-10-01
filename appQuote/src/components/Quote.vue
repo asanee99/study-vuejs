3d8cd6103be2eb8078fbdd4f8bed128f9d30d153
ASANEE <asanee.tan@gmail.com>
2018-10-01 16:43:08 +0700

    this app learn about control array and progressbar

---
+++ appQuote/src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body qoute">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+   
+</script>
+
+<style>
+    .panel-body {
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .qoute:hover {
+        background-color: #ffe2e2; 
+    }
+
+</style>
+
+
