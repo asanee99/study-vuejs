d013a06c928fda5df8affe35b68f6a56a1ce943e
ASANEE <asanee.tan@gmail.com>
2018-10-03 16:53:22 +0700

    asgm create own directive

---
+++ asgm-directive/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 v-mydiv:font="'cursive'">Directives Exercise</h1>
+                <h1 v-mydiv:font.intervaled="'cursive'">Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            'mydiv': {
+                bind(el, binding, vnode) {
+                    if (binding.modifiers['intervaled']) {
+                        setTimeout(() => {
+                            let nowFont = binding.value;
+                            let fontOne = 'fantasy';
+                            let fontTwo = 'monospace';
+                            let currentFont = binding.value;
+                            setInterval(() => {
+                                if ( currentFont == nowFont) {
+                                    currentFont = fontOne;
+                                } else if (currentFont == fontOne) {
+                                    currentFont = fontTwo;
+                                } else {
+                                    currentFont = nowFont;
+                                }
+                                el.style.fontFamily = currentFont;
+                            }, 1000);
+                        },3000);
+                        } else {
+                            if (binding.arg == ['font']) {
+                                el.style.fontFamily = binding.value;
+                            }
+                        }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
